d6ecbec3d72eae30775ef9d6f959e3835b2508a4
xkwd <34714644+xkwd@users.noreply.github.com>
2018-02-24 14:06:59 +0100

    Add user and author columns to opinions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180224125105) do
+ActiveRecord::Schema.define(version: 20180224125645) do
 
   create_table "languages", force: :cascade do |t|
     t.string "name", null: false
@@ -33,6 +33,10 @@ ActiveRecord::Schema.define(version: 20180224125105) do
     t.text "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.integer "author_id"
+    t.index ["author_id"], name: "index_opinions_on_author_id"
+    t.index ["user_id"], name: "index_opinions_on_user_id"
   end
 
   create_table "speakers", force: :cascade do |t|
