51aa82863d613795b808e7f4a4f873e6f9ddb0a6
xkwd <34714644+xkwd@users.noreply.github.com>
2018-02-24 14:55:39 +0100

    Add relations between users and opinions

--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -1,3 +1,6 @@
 class Opinion < ApplicationRecord
+  belongs_to :user, foreign_key: "user_id", class_name: "User"
+  belongs_to :author, foreign_key: "author_id", class_name: "User"
+
   validates :user, :content, :rating, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :technologies, through: :levels
   has_many :speakers
   has_many :languages, through: :speakers
+  has_many :received_opinions, foreign_key: "user_id", class_name: "Opinion"
+  has_many :written_opinions, foreign_key: "author_id", class_name: "Opinion"
+  
   validates :first_name, :last_name, :email, presence: true
 
   # Include default devise modules. Others available are:
