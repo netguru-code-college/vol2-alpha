e8d1ceca26120deb47bb31dfa088e7854dc7f490
Unknown <mieszko335@gmail.com>
2018-02-27 11:37:17 +0100

    Add an end.
    Hold your breath and count to ten.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,8 @@ class UsersController < ApplicationController
     @technologies = Technology.all
     @languages = Language.all.map { |c| [c.name, c.id] }
     redirect_to user_profile_index_path(@user)
-  
+  end 
+
   def show
     @user = User.find(params[:id])
     @technologies = @user.technologies
