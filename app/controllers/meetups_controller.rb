3829a75fbf61842ff33200242fbe845dc589e7f7
xkwd <34714644+xkwd@users.noreply.github.com>
2018-02-28 22:06:18 +0100

    Allow to search meetups by technology

--- app/controllers/meetups_controller.rb
+++ app/controllers/meetups_controller.rb
@@ -1,6 +1,11 @@
 class MeetupsController < ApplicationController
   def index
-    @meetups = Meetup.all
+    @technology_names = Technology.all.pluck(:name).sort
+    @meetups = if params[:query]
+                 Meetup.includes(:technology).where(technologies: { name: params[:query] })
+               else
+                 Meetup.all
+               end
     @hash = Gmaps4rails.build_markers(@meetups) do |meetup, marker|
       marker.lat meetup.lat
       marker.lng meetup.lng
