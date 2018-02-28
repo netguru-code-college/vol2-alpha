38189e7d3ff209c4d4b828d226d79a4790784962
Agnieszka <a.wojtowicz.23@gmail.com>
2018-02-28 09:55:17 +0100

    Add users' avatars

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
 module ApplicationHelper
-
+  def gravatar_url(email)
+      gravatar = Digest::MD5::hexdigest(email).downcase
+      url = "http://gravatar.com/avatar/#{gravatar}.png"
+    end
 end
